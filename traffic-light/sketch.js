192e2ee00cae2299817eb75ce6d0efb89d4b5bcc
Amy L. Zhang <167574187+amylzh07@users.noreply.github.com>
2024-09-25 10:29:47 -0600

    traffic light

---
+++ traffic-light/sketch.js
@@ -0,0 +1,62 @@
+// Traffic Light Simulator
+// Amy L Zhang
+
+let lightState = "green";
+let lastSwitchedTime = 0;
+const GREEN_LIGHT_DURATION = 3000;
+const YELLOW_LIGHT_DURATION = 500;
+const RED_LIGHT_DURATION = 3500;
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(255);
+  drawOutlineOfLights();
+  changeStateIfNeeded();
+  displayCorrectLight();
+}
+
+function changeStateIfNeeded() {
+  if (lightState === "green" && millis() > lastSwitchedTime + GREEN_LIGHT_DURATION) {
+    lightState = "yellow";
+    lastSwitchedTime = millis();
+  }
+  else if (lightState === "yellow" && millis() > lastSwitchedTime + YELLOW_LIGHT_DURATION) {
+    lightState = "red";
+    lastSwitchedTime = millis();
+  }
+  else if (lightState === "red" && millis() > lastSwitchedTime + RED_LIGHT_DURATION) {
+    lightState = "green";
+    lastSwitchedTime = millis();
+  }
+}
+
+function displayCorrectLight() {
+  if (lightState === "green") {
+    fill("green");
+    ellipse(width/2, height/2 + 65, 50, 50); // bottom;
+  }
+  else if (lightState === "yellow") {
+    fill("yellow");
+    ellipse(width/2, height/2, 50, 50); // middle 
+  }
+  else if (lightState === "red") {
+    fill("red");
+    ellipse(width/2, height/2 - 65, 50, 50); // top
+  }
+}
+
+function drawOutlineOfLights() {
+  // box
+  rectMode(CENTER);
+  fill(0);
+  rect(width/2, height/2, 75, 200, 10);
+  
+  // lights
+  fill(255);
+  ellipse(width/2, height/2 - 65, 50, 50); // top
+  ellipse(width/2, height/2, 50, 50); // middle 
+  ellipse(width/2, height/2 + 65, 50, 50); // bottom
+}
