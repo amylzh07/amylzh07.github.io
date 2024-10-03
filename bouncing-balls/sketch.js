bd260c19903a381a2ae301e4001e3a58a60a175c
Amy L. Zhang <167574187+amylzh07@users.noreply.github.com>
2024-10-03 10:55:33 -0600

    bouncing balls demo

--- bouncing-balls/sketch.js
+++ bouncing-balls/sketch.js
@@ -1,9 +1,11 @@
-// Project Title
-// Your Name
-// Date
+// Bouncing Balls
+// Amy L Zhang
+// Oct 3, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
+let ballArray = [];
 
 
 function setup() {
@@ -11,5 +13,44 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(200, 200, 256);
+
+  for (let theBall of ballArray) {
+    // move ball
+    theBall.x += theBall.dx;
+    theBall.y += theBall.dy;
+
+    // bounce if needed
+    if (theBall.x > width - theBall.radius || theBall.x < 0 + theBall.radius) {
+      theBall.dx *= -1;
+    }
+    else if (theBall.y > height - theBall.radius || theBall.y < 0 + theBall.radius) {
+      theBall.dy *= -1;
+    }
+
+    // show ball
+    noStroke();
+    fill(theBall.red, theBall.green, theBall.blue, theBall.alpha);
+    circle(theBall.x, theBall.y, theBall.radius * 2);
+  }
+}
+
+function mousePressed() {
+  spawnBall(mouseX, mouseY);
+}
+
+function spawnBall(theX, theY) {
+  let theBall = {
+    x: theX, 
+    y: theY,
+    radius: random(30, 60),
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+    red: random(255),
+    green: random(255),
+    blue: random(255),
+    alpha: random(180),
+  };
+
+  ballArray.push(theBall);
 }
