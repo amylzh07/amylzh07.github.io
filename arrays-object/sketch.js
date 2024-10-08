b9ccb32c32f8ebe0fc5673af5bce7933b3f9cb79
Amy L. Zhang <167574187+amylzh07@users.noreply.github.com>
2024-10-08 11:02:46 -0600

    created object project

---
+++ arrays-object/sketch.js
@@ -0,0 +1,18 @@
+// Arrays and Object Notation Assignment
+// Amy (Lening) Zhang
+// October 21, 2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+}
+
+function draw() {
+  background(0);
+  box(windowWidth, windowHeight);
+
+}
+
