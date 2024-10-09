b36529b8c80fde1a0f18d07ae451eff541ff04c0
Amy L. Zhang <167574187+amylzh07@users.noreply.github.com>
2024-10-09 11:03:18 -0600

    added keyboard controls

--- arrays-object/sketch.js
+++ arrays-object/sketch.js
@@ -4,7 +4,10 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let posZ = 100;
+let posX = 100;
+let dx = 15;
+let dz = 15;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -12,7 +15,26 @@ function setup() {
 
 function draw() {
   background(0);
-  box(windowWidth, windowHeight);
+  //  box(windowWidth, windowHeight);
 
+  box(100, 100);
+  translate(posX, 0, posZ);
+  moveCharacter();
 }
 
+function moveCharacter() {
+  if (keyIsDown(87)) { // move further (Z-axis)
+    posZ += dz;
+  }
+  else if (keyIsDown(83)){ // move closer (Z-axis)
+    posZ -= dz;
+  }
+  else if (keyIsDown(68)){ // move right (X-axis)
+    posX += dx;
+  }
+  else if (keyIsDown(65)){ // move left(X-axis)
+    posX -= dx;
+  }
+}
+
+// translate while moving. when stopped and displaying, push()
