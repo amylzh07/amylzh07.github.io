414048cb284f7f2886dba68c3baaabca32f90db8
Amy L. Zhang <167574187+amylzh07@users.noreply.github.com>
2024-10-10 10:45:50 -0600

    bubble demo with perlin noise

--- bubbles/sketch.js
+++ bubbles/sketch.js
@@ -2,26 +2,79 @@
 // Removing objects from the array
 
 let theBubbles = [];
+let deathLocations = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 
   for (i = 0; i < 5; i++) {
     spawnBubble();
   }
+
+  // create a new bubble
+  window.setInterval(spawnBubble, 500);
 }
 
 function draw() {
-  background(220, 220, 255);
-  moveBubblesRandomly();
+  background(250);
+  moveBubblesWithNoise();
   displayBubbles();
+  displayDeathSpots();
+}
+
+function mousePressed() {
+  for (let bubble of theBubbles) {
+    if (clickedInBubble(mouseX, mouseY, bubble)) {
+      let theIndex = theBubbles.indexOf(bubble);
+      theBubbles.splice(theIndex, 1);
+      addDeath(mouseX, mouseY);
+    }
+  }
+}
+
+function displayDeathSpots() {
+  for (let spot of deathLocations) {
+    // add images for bubble pop
+  }
+}
+
+function addDeath(_x, _y) {
+  let deathSpot = {
+    x: _x,
+    y: _y,
+  };
+  deathLocations.push(deathSpot);
+}
+
+
+function clickedInBubble(x, y, theBubble) {
+  let distanceAway = dist(x, y, theBubble.x, theBubble.y);
+  if (distanceAway < theBubble.radius) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function displayBubbles() {
   for (let bubble of theBubbles) {
-    noStroke();
     fill(bubble.red, bubble.green, bubble.blue, bubble.alpha);
     circle(bubble.x, bubble.y, bubble.radius * 2);
+  }
+}
+
+function moveBubblesWithNoise() {
+  for (let bubble of theBubbles) {
+    let x = noise(bubble.timeX) * width;
+    let y = noise(bubble.timeY) * height;
+
+    bubble.x = x;
+    bubble.y =y;
+    
+    bubble.timeX += bubble.deltaTime;
+    bubble.timeY += bubble.deltaTime;
   }
 }
 
@@ -56,6 +109,9 @@ function spawnBubble() {
     green: random(255),
     blue: random(255),
     alpha: random(255),
+    timeX: random(1000000),
+    timeY: random(1000000),
+    deltaTime: 0.002,
   };
   theBubbles.push(someBubble);
 }
