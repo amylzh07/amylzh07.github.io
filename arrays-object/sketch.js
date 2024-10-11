061b2cc083dae4e47af4c5effbb650786d8292f5
Amy L. Zhang <167574187+amylzh07@users.noreply.github.com>
2024-10-11 10:05:51 -0600

    add camera and physics for jump

--- arrays-object/sketch.js
+++ arrays-object/sketch.js
@@ -4,10 +4,12 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let posZ = 100;
-let posX = 100;
-let dx = 15;
-let dz = 15;
+let posZ = 0;
+let posX = 0;
+let dx = 5;
+let dz = 5;
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -18,24 +20,24 @@ function draw() {
 
   background(0);
   push();
-  translate(0, -windowHeight, 0);
+  translate(0, windowHeight / 2, 0);
   fill(220);
   box(windowWidth, windowHeight);
   pop();
 
   fill(0);
   translate(posX, 0, posZ);
-  box(100, 100);
+  box(10, 10);
 
   moveCharacter();
 
 }
 
 function moveCharacter() {
-  if (keyIsDown(87)) { // move further (Z-axis)
+  if (keyIsDown(83)) { // move further (Z-axis)
     posZ += dz;
   }
-  else if (keyIsDown(83)){ // move closer (Z-axis)
+  else if (keyIsDown(87)){ // move closer (Z-axis)
     posZ -= dz;
   }
   else if (keyIsDown(68)){ // move right (X-axis)
