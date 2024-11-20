f86e520e0db36b2d7053b337459bac5a39f45610
Amy L. Zhang <167574187+amylzh07@users.noreply.github.com>
2024-11-20 10:53:18 -0600

    finished nodes demo

--- connected-nodes/sketch.js
+++ connected-nodes/sketch.js
@@ -10,16 +10,22 @@ function setup() {
 
 function draw() {
   background(50);
+
+  // draw lines first
   for (let point of points) {
-    point.wrapAroundScreen();
-    point.move();
-    point.display();
+    point.update();
+    point.connectTo(points);
   }
+
+  // draw circles afterwards to be on top
+//  for (let point of points) {
+//    point.display();
+//  }
 }
 
 function mousePressed() {
-  let newPoint = new MovingPoint(mouseX, mouseY);
-  points.push(newPoint);
+  let somePoint = new MovingPoint(mouseX, mouseY);
+  points.push(somePoint);
 }
 
 class MovingPoint {
@@ -32,12 +38,48 @@ class MovingPoint {
     this.xTime = random(1000);
     this.yTime = random(1000);
     this.deltaTime = 0.01;
+    this.reach = 150;
+    this.minRadius = 25;
+    this.maxRadius = 70;
   }
+
   display() {
     noStroke();
     fill(this.color);
     circle(this.x, this.y, this.radius);
   }
+
+  update() {
+    this.move();
+    this.wrapAroundScreen();
+    this.adjustSizeWithMouse();
+  }
+
+  connectTo(pointsArray) {
+    for (let otherPoint of pointsArray) {
+      // avoid drawing line with self
+      if (this !== otherPoint) {
+        let pointDistance = dist(this.x, this.y, otherPoint.x, otherPoint.y);
+        if (pointDistance < this.reach) {
+          stroke(this.color);
+          line(this.x, this.y, otherPoint.x, otherPoint.y);
+        }
+      }
+    }
+  }
+
+  adjustSizeWithMouse() {
+    let mouseDistance = dist(this.x, this.y, mouseX, mouseY);
+
+
+    if (mouseDistance < this.reach) {
+      this.radius = map(mouseDistance, 0, this.reach, this.maxRadius, this.minRadius);
+    }
+    else {
+      this.radius = this.minRadius;
+    }
+  }
+
   move() {
     // pick random direction of movement
     let dx = noise(this.xTime);
@@ -58,16 +100,16 @@ class MovingPoint {
   wrapAroundScreen() {
     // wrap around screen if you fall off
     if (this.x > width) {
-      this.x = 0;
+      this.x -= width;
     }
     if (this.x < 0) {
-      this.x = width;
+      this.x += width;
     }
     if (this.y > height) {
-      this.y = 0;
+      this.y -= height;
     }
     if (this.y < 0) {
-      this.y = height;
+      this.y += height;
     }
   }
 }
